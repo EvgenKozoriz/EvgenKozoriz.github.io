753a4f4595d8362aba94aeef46a05d806e9cfbd2
EvgenKozoriz <68121704+EvgenKozoriz@users.noreply.github.com>
2020-07-19 14:16:41 +0300

    first commit

---
+++ third/scripts/script.js
@@ -0,0 +1,15 @@
+/**
+ * @param {obj} elementObj
+ * @param {int} intPaddingTop, px
+ * @param {int} intSpeedScroll, ms
+ */
+function scrollToElement(elementObj, intPaddingTop, intSpeedScroll) {
+ 
+    intPaddingTop = intPaddingTop || 100; /* Default 10px */
+    intSpeedScroll = intSpeedScroll || 500 /* Default 200ms */
+    var scrollTop = $(elementObj).offset().top - intPaddingTop;
+  
+    $('html, body').animate({
+      scrollTop: scrollTop
+    }, intSpeedScroll);
+  }
